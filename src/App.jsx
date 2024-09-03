ae80f3a98f411c0252555a83d8d5403d271c46da
Fasilfaz <fazsby2001@gmail.com>
2024-09-03 21:09:42 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import{ useState } from 'react';
+import './app.css';
+
+function App() {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState('');
+
+  const handleAddTodo = () => {
+    if (newTodo.trim() !== '') {
+      setTodos([...todos, newTodo.trim()]);
+      setNewTodo('');
+    }
+  };
+
+  const handleDeleteTodo = (index) => {
+    const updatedTodos = todos.filter((_, i) => i !== index);
+    setTodos(updatedTodos);
+  };
+
+  return (
+    <div >
+      <h1>Todo App</h1>
+      <div className="todo-input">
+        <input
+          type="text"
+          placeholder="Add a new task"
+          value={newTodo}
+          onChange={(e) => setNewTodo(e.target.value)}
+        />
+        <button onClick={handleAddTodo}>Add</button>
+      </div>
+      <ul className="todo-list">
+        {todos.map((todo, index) => (
+          <li key={index} className="todo-item">
+            {todo}
+            <button onClick={() => handleDeleteTodo(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
